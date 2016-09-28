44023bb86d77a51c58007a1254a0055ee12c477b
dipdeb <netro.sai@gmail.com>
2016-09-28 07:50:37 +0000

    JS1

--- server.js
+++ server.js
@@ -2,6 +2,82 @@ var express = require('express');
 var morgan = require('morgan');
 var path = require('path');
 
+var articles = {
+	'article-one': {
+		title: "Article one",
+		heading: "Article one",
+		date: "5-sep-2016",
+		content: `
+			<p>
+				This is  the content of article one.This is  the content of article one.This is  the content of article one.This is  the content of article one.This is  the content of article one.This is  the content of article one.This is  the content of article one.
+			</p>    
+			<p>
+				This is  the content of article one.This is  the content of article one.This is  the content of article one.This is  the content of article one.This is  the content of article one.This is  the content of article one.This is  the content of article one.
+			</p>    
+			<p>
+				This is  the content of article one.This is  the content of article one.This is  the content of article one.This is  the content of article one.This is  the content of article one.This is  the content of article one.This is  the content of article one.
+			</p>    
+			<p>
+				This is  the content of article one.This is  the content of article one.This is  the content of article one.This is  the content of article one.This is  the content of article one.This is  the content of article one.This is  the content of article one.
+			</p>    
+	       `
+	},
+	'article-two': {
+		title: "Article two",
+		heading: "Article two",
+		date: "15-sep-2016",
+		content: `
+			<p>
+				This is  the content of article two.This is  the content of article two.
+			</p>    
+	       `
+	},
+	'article-three': {
+		title: "Article three",
+		heading: "Article three",
+		date: "30-sep-2016",
+		content: `
+			<p>
+				This is  the content of article two.This is  the content of article two.
+			</p>    
+	       `
+	}
+};
+
+function createTemplate (data) {
+	var heading = data.heading;
+	var title = data.title;
+	var date = data.date;
+	var content = data.content;
+
+	var htmlTemplate = 
+	`
+		<html>
+			<head>
+					<title>${title}</title>
+					<link href="/ui/style.css" rel="stylesheet" />
+			</head>
+			<body>				
+				<div class="container">
+					<div>
+						<a href="/">Home</a>    
+						<a href="/article-one">Article One</a>    
+						<a href="/article-two">Article Two</a>    
+						<a href="/article-three">Article Three</a>    
+					</div>    
+					<hr/>
+					<h3>${heading}</h3>
+					<div>${date}</div>    
+					<div>
+						${content}
+					</div>		
+				</div>
+			</body>
+		</html>
+		`;
+	return htmlTemplate;
+}
+
 var app = express();
 app.use(morgan('combined'));
 
@@ -9,8 +85,14 @@ app.get('/', function (req, res) {
   res.sendFile(path.join(__dirname, 'ui', 'index.html'));
 });
 
-app.get('/article-one', function (req, res) {
-   res.sendFile(path.join(__dirname, 'ui', 'article-one.html')); 
+app.get('/:articleName', function (req, res) {
+	var articleName = req.params.articleName;	
+	res.send(createTemplate(articles[articleName]));
+   //res.sendFile(path.join(__dirname, 'ui', 'article-one.html')); 
+});
+
+app.get('/ui/main.js', function (req, res) {
+  res.sendFile(path.join(__dirname, 'ui', 'main.js'));
 });
 
 app.get('/ui/style.css', function (req, res) {
