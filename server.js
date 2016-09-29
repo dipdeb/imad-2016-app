5bbf799cb337da748ea25c4438350b57d24c93b4
Dipanjan Deb <netro.sai@gmail.com>
2016-09-29 18:42:22 +0530

    [imad-console] Updates server.js

--- server.js
+++ server.js
@@ -123,8 +123,8 @@ app.get('/ui/madi.png', function (req, res) {
   res.sendFile(path.join(__dirname, 'ui', 'madi.png'));
 });
 
-var port = 2400; // Use 8080 for local development because you might already have apache running on 80
+var port = 8080; // Use 8080 for local development because you might already have apache running on 80
 
-app.listen(2400, function () {
+app.listen(8080, function () {
   console.log(`IMAD course app listening on port ${port}!`);
 });
