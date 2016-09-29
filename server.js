793acd71b2f5cec0fe5c028466fd59019d0b9bb8
dipdeb <netro.sai@gmail.com>
2016-09-29 12:46:38 +0000

    JS1

--- server.js
+++ server.js
@@ -2,109 +2,129 @@ var express = require('express');
 var morgan = require('morgan');
 var path = require('path');
 
+var app = express();
+app.use(morgan('combined'));
+
 var articles = {
-	'article-one': {
-		title: "Article one",
-		heading: "Article one",
-		date: "5-sep-2016",
-		content: `
-			<p>
-				This is  the content of article one.This is  the content of article one.This is  the content of article one.This is  the content of article one.This is  the content of article one.This is  the content of article one.This is  the content of article one.
-			</p>    
-			<p>
-				This is  the content of article one.This is  the content of article one.This is  the content of article one.This is  the content of article one.This is  the content of article one.This is  the content of article one.This is  the content of article one.
-			</p>    
-			<p>
-				This is  the content of article one.This is  the content of article one.This is  the content of article one.This is  the content of article one.This is  the content of article one.This is  the content of article one.This is  the content of article one.
-			</p>    
-			<p>
-				This is  the content of article one.This is  the content of article one.This is  the content of article one.This is  the content of article one.This is  the content of article one.This is  the content of article one.This is  the content of article one.
-			</p>    
-	       `
-	},
-	'article-two': {
-		title: "Article two",
-		heading: "Article two",
-		date: "15-sep-2016",
-		content: `
-			<p>
-				This is  the content of article two.This is  the content of article two.
-			</p>    
-	       `
-	},
-	'article-three': {
-		title: "Article three",
-		heading: "Article three",
-		date: "30-sep-2016",
-		content: `
-			<p>
-				This is  the content of article two.This is  the content of article two.
-			</p>    
-	       `
-	}
+    'article-one': {
+      title: 'Article One | XYZ',
+      heading: 'Article One',
+      date: 'Sep 5, 2016',
+      content: 
+          '<p>'+
+              'This is the content for my first article. This is the content for my first article. This is the content for my first article. This is the content for my first article. This is the content for my first article. This is the content for my first article.' + 
+          '</p>' +
+          '<p>' +
+              'This is the content for my first article. This is the content for my first article. This is the content for my first article. This is the content for my first article. This is the content for my first article. This is the content for my first article. ' +
+          '</p>' +
+          '<p>' +
+              'This is the content for my first article. This is the content for my first article. This is the content for my first article. This is the content for my first article. This is the content for my first article. This is the content for my first article. ' +
+          '</p>'
+    },
+    'article-two': {
+      title: 'Article Two | XYZ',
+      heading: 'Article Two',
+      date: 'Sep 10, 2016',
+      content: 
+          '<p>' +
+              'This is the content for my second article.' +
+          '</p>'
+    },
+    'article-three': {
+      title: 'Article Three | XYZ',
+      heading: 'Article Three',
+      date: 'Sep 15, 2016',
+      content: 
+          `<p>
+              This is the content for my third article.
+          </p>`
+    }
 };
 
 function createTemplate (data) {
-	var heading = data.heading;
-	var title = data.title;
-	var date = data.date;
-	var content = data.content;
-
-	var htmlTemplate = 
-	`
-		<html>
-			<head>
-					<title>${title}</title>
-					<link href="/ui/style.css" rel="stylesheet" />
-			</head>
-			<body>				
-				<div class="container">
-					<div>
-						<a href="/">Home</a>    
-						<a href="/article-one">Article One</a>    
-						<a href="/article-two">Article Two</a>    
-						<a href="/article-three">Article Three</a>    
-					</div>    
-					<hr/>
-					<h3>${heading}</h3>
-					<div>${date}</div>    
-					<div>
-						${content}
-					</div>		
+    var title = data.title;
+    var date = data.date;
+    var heading = data.heading;
+    var content = data.content;
+    
+    var htmlTemplate = 
+    `<html>  
+      <head> 
+          <title>  
+              ${title}
+          </title> 
+          <meta name="viewport" content="width=device-width, initial-scale=1" /> 
+          <link href="/ui/style.css" rel="stylesheet" />  
+      </head>  
+      <body> 
+          <div class="container"> 
+              <div> 
+                  <a href="/">Home</a> 
+              </div> 
+              <hr/> 
+              <h3> 
+                  ${heading}
+              </h3> 
+              <div> 
+                  ${date}
+              </div> 
+              <div> 
+                ${content}
+              </div> 
+				<div>
+					Enter your comment: <input type="text" id="comment" maxlength="20"></input>
+					<button id="sbmt_btn">Submit</button>
 				</div>
-			</body>
-		</html>
-		`;
-	return htmlTemplate;
+				<div id="box" style="display:none; border: 1px solid #D3D3D3;margin-top: 4px;background-color:#FFFFD4;max-height:150px;width:250px;overflow:auto;">
+				</div>
+          </div> 
+        <script type="text/javascript" src="/ui/main.js"></script>
+      </body> 
+    </html>`
+    ;
+    return htmlTemplate;
 }
-
-var app = express();
-app.use(morgan('combined'));
 
 app.get('/', function (req, res) {
   res.sendFile(path.join(__dirname, 'ui', 'index.html'));
 });
 
-app.get('/:articleName', function (req, res) {
-	var articleName = req.params.articleName;	
-	res.send(createTemplate(articles[articleName]));
-   //res.sendFile(path.join(__dirname, 'ui', 'article-one.html')); 
+var counter = 0;
+app.get('/counter', function (req, res) {
+   counter = counter + 1;
+   res.send(counter.toString());
 });
 
-app.get('/ui/main.js', function (req, res) {
-  res.sendFile(path.join(__dirname, 'ui', 'main.js'));
+var comments = [];
+app.get('/submit-comment', function(req, res) { 
+  // Get the name from the request
+  var comment = req.query.comment;
+  
+  comments.push(comment);
+  // JSON: Javascript Object Notation
+  res.send(JSON.stringify(comments));
+});
+
+app.get('/:articleName', function (req, res) {
+  var articleName = req.params.articleName;
+  if (articleName != 'favicon.ico')	
+  res.send(createTemplate(articles[articleName]));
 });
 
 app.get('/ui/style.css', function (req, res) {
   res.sendFile(path.join(__dirname, 'ui', 'style.css'));
 });
 
+app.get('/ui/main.js', function (req, res) {
+  res.sendFile(path.join(__dirname, 'ui', 'main.js'));
+});
+
 app.get('/ui/madi.png', function (req, res) {
   res.sendFile(path.join(__dirname, 'ui', 'madi.png'));
 });
 
+var port = 2400; // Use 8080 for local development because you might already have apache running on 80
 
-var port =2400; // Use 8080 for local development because you might already have apache running on 80
 app.listen(2400, function () {
   console.log(`IMAD course app listening on port ${port}!`);
 });
--- ui/main.js
+++ ui/main.js
@@ -1,1 +1,54 @@
-console.log('Loaded!');
+var btn = document.getElementById('counter');
+
+if (btn != undefined) {
+btn.onclick = function() {
+
+	var req = new XMLHttpRequest();
+
+	req.onreadystatechange = function() {
+		if (req.readyState === XMLHttpRequest.DONE) {
+			if (req.status === 200) {
+				var counter = req.responseText;
+				var spn = document.getElementById('count');
+				spn.innerHTML = counter;
+			}
+		}
+	};
+
+	req.open('GET', 'http://ssh.imad.hasura-app.io:2400/counter', true);
+	req.send(null);
+};
+}
+var sbmt_btn = document.getElementById('sbmt_btn');
+
+if (sbmt_btn != undefined) {
+sbmt_btn.onclick = function () {
+	var commentEl = document.getElementById('comment');
+	var comment = commentEl.value;
+	commentEl.value = '';
+	
+	var req = new XMLHttpRequest();
+
+	req.onreadystatechange = function() {
+		if (req.readyState === XMLHttpRequest.DONE) {
+			if (req.status === 200) {
+				var comments = req.responseText;
+				comments = JSON.parse(comments);
+
+				var commentBox = document.getElementById('box');
+				commentBox.style.display = "block";
+
+				var tmp = '';
+				for (var i = 0; i< comments.length; i++) {
+					 tmp += comments[i]+'<br/>';
+				}
+
+				commentBox.innerHTML = tmp;
+			}
+		}
+	};
+	
+	req.open('GET', 'http://ssh.imad.hasura-app.io:2400/submit-comment?comment='+comment, true);
+	req.send(null);
+};
+}
