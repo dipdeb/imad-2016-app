a8e154e3e61bb4a459e78564d73f4b1697bc93d7
Dipanjan Deb <netro.sai@gmail.com>
2016-10-25 23:12:31 +0530

    [imad-console] Updates server.js

--- server.js
+++ server.js
@@ -149,6 +149,7 @@ app.get('/:articleName', function (req, res) {
 });
 
 app.get('/articles/:articleName', function (res, req) {
+    console.log(req.params.articleName);
     pool.query("select * from article where title = '" + req.params.articleName + "'", function (err, result) {
         if (err)
             res.status(500).send(err.toString());
