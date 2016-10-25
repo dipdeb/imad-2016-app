a5ad813d8b40e3027b6ac1efd62b3685a8e619f3
Dipanjan Deb <netro.sai@gmail.com>
2016-10-25 23:07:02 +0530

    [imad-console] Updates server.js

--- server.js
+++ server.js
@@ -148,6 +148,23 @@ app.get('/:articleName', function (req, res) {
   }	
 });
 
+app.get('/articles/:articleName', function (res, req) {
+    pool.query("select * from test where name = '" + req.params.articleName + "'", function (err, result) {
+        if (err)
+            res.status(500).send(err.toString());
+        else {
+            if (result.rows.length === 0) 
+                res.status(404).send('Article not found');
+            else {
+                var articleData = result.rows[0];
+                res.send(createTemplate(articleData));
+            }  
+    
+        }   
+            
+    });
+});
+
 app.get('/ui/main.js', function (req, res) {
   res.sendFile(path.join(__dirname, 'ui', 'main.js'));
 });
