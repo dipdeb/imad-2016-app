321311b738c924bb00441d292c938f84e66d664e
Dipanjan Deb <netro.sai@gmail.com>
2016-10-25 22:25:45 +0530

    [imad-console] Updates server.js

--- server.js
+++ server.js
@@ -1,6 +1,14 @@
 var express = require('express');
 var morgan = require('morgan');
 var path = require('path');
+var Pool = require('pg').Pool;
+var config = {
+    user: 'dipanjan',
+    database: 'dipanjan',
+    host: 'db.imad.hasura-app.io',
+    port: 5432,
+    password: process.env.DB_PASSWORD
+}
 
 var app = express();
 app.use(morgan('combined'));
@@ -78,6 +86,16 @@ function createTemplate (data) {
 
 app.get('/', function (req, res) {
   res.sendFile(path.join(__dirname, 'ui', 'index.html'));
+});
+
+var pool = new Pool(config);
+app.get('/test-db', function(req, res) {
+    pool.query("select * from test", function (err, result) {
+        if (err)
+            res.status(500).send(err.toString());
+        else    
+            res.send(JSON.string(result));
+    });
 });
 
 var counter = 0;
