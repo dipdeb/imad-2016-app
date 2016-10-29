0d927fb55f81f36fe691b1578cde90afceb9d849
dipdeb <netro.sai@gmail.com>
2016-10-29 18:10:29 +0530

    removed unwanted files

--- server.js
+++ server.js
@@ -35,8 +35,8 @@ function createTemplate (data) {
 	return htmlTemplate;
 }
 
-app.get('/ui/dashboard.css', function (req, res) {
-  res.sendFile(path.join(__dirname, 'ui', 'dashboard.css'));
+app.get('/ui/style.css', function (req, res) {
+  res.sendFile(path.join(__dirname, 'ui', 'style.css'));
 });
 
 app.get('/', function (req, res) {
