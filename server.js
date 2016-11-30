c5d0aaad7eb5a33d8b977e090c0bd8a438934438
dipdebx <deb.dipanjan@gmail.com>
2016-11-30 13:05:37 +0530

    heroku supp

--- server.js
+++ server.js
@@ -8,10 +8,8 @@ var session = require('express-session');
 
 const url = require('url')
 
-if (process.env.DATABASE_URL) {
 	const params = url.parse(process.env.DATABASE_URL);
 	const auth = params.auth.split(':');
-}
 
 var config = {
 	/*user: auth[0],
